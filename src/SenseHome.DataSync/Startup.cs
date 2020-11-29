b71707370ffa09dc4f0b61f4504e80c3a89c98f2
Rafiul Islam <avoidcloud@gmail.com>
2020-11-29 17:51:21 +0600

    configured serilog service to logging

--- src/SenseHome.DataSync/Startup.cs
+++ src/SenseHome.DataSync/Startup.cs
@@ -22,6 +22,7 @@ namespace SenseHome.DataSync
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMqttClientHostedService(Configuration);
+            services.AddSenseHomeDataLoggingService(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
